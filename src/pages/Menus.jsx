9f7ecf5f5db0d62f590e6a5f7ed986836afbea86
Andy Chang <andy.rochi@gmail.com>
2023-06-05 19:14:23 +0200

    feat: Add category navigation bar

--- src/pages/Menus.jsx
+++ src/pages/Menus.jsx
@@ -1,17 +1,18 @@
 import React, { useState } from "react";
 
-import { Container, Row, Col } from "reactstrap";
+import { Container, Row, Col, Nav, NavItem, NavLink } from "reactstrap";
 
 import products from "../assets/fake-data/products";
 import ProductCard from "../components/UI/product-card/ProductCard";
 import Helmet from "../components/Helmet/Helmet";
-import ReactPaginate from "react-paginate";
 import "../styles/pagination.css";
 
 import { categoryListApi } from "../api/main";
 
 const Menus = () => {
   const [pageNumber, setPageNumber] = useState(0);
+  const [categoryList, setCategoryList] = useState([]);
+  const [activeCategory, setActiveCategory] = useState("");
 
   const searchedProduct = products;
 
@@ -32,6 +33,8 @@ const Menus = () => {
     categoryListApi()
       .then((res) => {
         console.log(res, res.data);
+        setCategoryList(res.data);
+        setActiveCategory(res.data[0].id);
       })
       .catch((err) => {
         alert('request error.');
@@ -41,7 +44,24 @@ const Menus = () => {
 
   return (
     <Helmet title="Menu">
+      <Nav pills className="justify-content-between">
+        {categoryList.map((cat) =>
+        (<NavItem key={cat.id}>
+          <NavLink className="custom-link" href="#" active={cat.id === activeCategory}
+            onClick={(event) => {
+              event.preventDefault();
+              setActiveCategory(cat.id);
+            }}>
+            {cat.name}
+          </NavLink>
+        </NavItem>))
+        }
+      </Nav>
       <Container>
+        <Row className="justify-content-between">
+
+          {/* {categoryList.map((cat) => (<div key={cat.id}>{cat.name}</div>))} */}
+        </Row>
         <Row>
           {displayPage.map((item) => (
             <Col
@@ -55,7 +75,7 @@ const Menus = () => {
               <ProductCard item={item} />
             </Col>
           ))}
-          <div className="d-flex justify-content-center mt-4 mb-4">
+          {/* <div className="d-flex justify-content-center mt-4 mb-4">
             <ReactPaginate
               pageCount={pageCount}
               onPageChange={changePage}
@@ -63,7 +83,7 @@ const Menus = () => {
               nextLabel={"Next"}
               containerClassName="paginationBttns"
             />
-          </div>
+          </div> */}
         </Row>
       </Container>
     </Helmet>
