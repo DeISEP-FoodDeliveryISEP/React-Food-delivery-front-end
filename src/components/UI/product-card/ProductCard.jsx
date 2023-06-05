78e0b75826b2bcc47977f549571ab7dcaa03c6a1
Andy Chang <andy.rochi@gmail.com>
2023-06-05 19:32:30 +0200

    feat: implement item fetch function

--- src/components/UI/product-card/ProductCard.jsx
+++ src/components/UI/product-card/ProductCard.jsx
@@ -10,15 +10,15 @@ import { cartActions } from "../../../store/shopping-cart/cartSlice";
 import { Link } from "react-router-dom";
 
 const ProductCard = (props) => {
-  const { id, title, image01, price, extraIngredients } = props.item;
+  const { id, name, image, price, extraIngredients } = props.item;
   const dispatch = useDispatch();
 
   const addToCart = () => {
     dispatch(
       cartActions.addItem({
         id,
-        title,
-        image01,
+        name,
+        image,
         price,
         extraIngredients
       })
@@ -28,13 +28,18 @@ const ProductCard = (props) => {
   return (
     <div className="product__item d-flex flex-column justify-content-between">
       <div className="product__content">
-        <img className="product__img w-50" src={image01} alt="Pizza" />
+        <img className="product__img w-50" src={image} alt="food" />
         <h5>
-          <Link to={`/pizzas/${id}`}>{title}</Link>
+          <Link to={`/pizzas/${id}`}>{name}</Link>
         </h5>
       </div>
       <div className="d-flex flex-column align-items-center justify-content-between">
-        <span className="product__price mb-2">{price} € </span>
+        <span className="product__price mb-2">
+          {new Intl.NumberFormat('en-US', {
+            style: 'currency',
+            currency: 'EUR',
+          }).format(price / 100)}
+        </span>
         <button className="addTOCART__btn" onClick={addToCart}>
           Add to Cart
         </button>
--- src/pages/Menus.jsx
+++ src/pages/Menus.jsx
@@ -2,32 +2,32 @@ import React, { useState } from "react";
 
 import { Container, Row, Col, Nav, NavItem, NavLink } from "reactstrap";
 
-import products from "../assets/fake-data/products";
+// import products from "../assets/fake-data/products";
 import ProductCard from "../components/UI/product-card/ProductCard";
 import Helmet from "../components/Helmet/Helmet";
 import "../styles/pagination.css";
 
-import { categoryListApi } from "../api/main";
+import { categoryListApi, dishListApi } from "../api/main";
 
 const Menus = () => {
   const [pageNumber, setPageNumber] = useState(0);
   const [categoryList, setCategoryList] = useState([]);
   const [activeCategory, setActiveCategory] = useState("");
+  const [displayItems, setDisplayItems] = useState([]);
+  // const searchedProduct = products;
 
-  const searchedProduct = products;
+  // const productPerPage = 4;
+  // const visitedPage = pageNumber * productPerPage;
+  // const displayPage = searchedProduct.slice(
+  //   visitedPage,
+  //   visitedPage + productPerPage
+  // );
 
-  const productPerPage = 4;
-  const visitedPage = pageNumber * productPerPage;
-  const displayPage = searchedProduct.slice(
-    visitedPage,
-    visitedPage + productPerPage
-  );
+  // const pageCount = Math.ceil(searchedProduct.length / productPerPage);
 
-  const pageCount = Math.ceil(searchedProduct.length / productPerPage);
-
-  const changePage = ({ selected }) => {
-    setPageNumber(selected);
-  };
+  // const changePage = ({ selected }) => {
+  //   setPageNumber(selected);
+  // };
 
   React.useEffect(() => {
     categoryListApi()
@@ -41,6 +41,18 @@ const Menus = () => {
         console.error(err);
       })
   }, []);
+
+  React.useEffect(() => {
+    dishListApi({ categoryId: activeCategory, status: 1 })
+      .then((res) => {
+        console.log(res, res.data);
+        setDisplayItems(res.data);
+      })
+      .catch((err) => {
+        alert('request error.');
+        console.error(err);
+      })
+  }, [activeCategory]);
 
   return (
     <Helmet title="Menu">
@@ -63,7 +75,7 @@ const Menus = () => {
           {/* {categoryList.map((cat) => (<div key={cat.id}>{cat.name}</div>))} */}
         </Row>
         <Row>
-          {displayPage.map((item) => (
+          {displayItems.map((item) => (
             <Col
               lg="3"
               md="4"
@@ -75,15 +87,6 @@ const Menus = () => {
               <ProductCard item={item} />
             </Col>
           ))}
-          {/* <div className="d-flex justify-content-center mt-4 mb-4">
-            <ReactPaginate
-              pageCount={pageCount}
-              onPageChange={changePage}
-              previousLabel={"Prev"}
-              nextLabel={"Next"}
-              containerClassName="paginationBttns"
-            />
-          </div> */}
         </Row>
       </Container>
     </Helmet>
